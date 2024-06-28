09ff9dacad9f2f67a1367253dfc6469cd4a8e14f
hamalwa <hamalwa@neubaukompass.de>
2024-06-28 18:43:02 +0200

    <<>>

--- tina/config.ts
+++ tina/config.ts
@@ -3,11 +3,12 @@ import Post from "./collections/post";
 
 export default defineConfig({
   clientId: process.env.TINA_CLIENT_ID!,
+  token: process.env.TINA_TOKEN!,
   branch:
     process.env.TINA_BRANCH! || // custom branch env override
     process.env.VERCEL_GIT_COMMIT_REF! || // Vercel branch env
     process.env.HEAD!, // Netlify branch env
-  token: process.env.TINA_TOKEN!,
+
   build: {
     outputFolder: "admin",
     publicFolder: "static",
